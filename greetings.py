7ae28fba7b4a78a42310e4c0354800e00ac0c218
Andreipoh <pohlebaevandrei@yandex.ru>
2024-12-23 18:19:58 +0500

    greetings.py

--- greetings.py
+++ greetings.py
@@ -4,3 +4,8 @@ content =
 status =
 created_date = "22-12-2024"
 issue_date = "22-01-2025"
+temp_created_date = "22.12"
+temp_issue_date = "22.01"
+print(temp_created_date)
+print(temp_issue_date)
+
