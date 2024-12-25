121047fb5ac7a43aaf0c3ea6813325499d5348a6
Andreipoh <pohlebaevandrei@yandex.ru>
2024-12-25 20:26:52 +0500

    Add files via upload

---
+++ add_list.py
@@ -0,0 +1,8 @@
+headlines = []
+a = input('Введите заголовок: ')
+b = input('Введите заголовок: ')
+c = input('Введите заголовок: ')
+headlines.append(a)
+headlines.append(b)
+headlines.append(c)
+print('Заголовки', headlines)
