fb98b9fcd6d1b9db870814974dc90e3ca8b7175f
Andreipoh <pohlebaevandrei@yandex.ru>
2024-12-31 21:13:46 +0500

    check_deadline.py

--- check_deadline.py
+++ check_deadline.py
@@ -1,12 +1,17 @@
-import datetime as dt
-def normalize_date(date_str):
-    return date_str.replace(".", "-").replace("/", "-")
+from datetime import date
 
-issue_date_str = normalize_date(input('Введите дату истечения заметки в формате "день-месяц-год" "xx-xx-xxxx": '))
-create_date = dt.datetime.now().strftime("%d-%m-%Y")
-create_date = dt.datetime.strptime(create_date, "%d-%m-%Y")
-issue_date  = dt.datetime.strptime(issue_date_str, "%d-%m-%Y")
-if issue_date >= create_date:
-    print(f"Внимание! Дедлайн истёк {issue_date - create_date} дня назад.")
+day,month,year = input('Введите дату дедлайна (в формате день-месяц-год "xx-xx-xxxx"): ').split('-')
+issue_date = (f'{day}-{month}-{year}')
+year1, month1, day1= str(date.today()).split('-')
+print(day1,month1,year1)
+if ((int(day1) > int(day) and int(month1) == int(month) and int(year1) == int(year)) or
+    (int(day1) == int(day) and int(month1) > int(month) and int(year1) == int(year)) or
+    (int(day1) == int(day) and int(month1) == int(month) and int(year1) > int(year)) or
+    (int(day1) > int(day) and int(month1) > int(month) and int(year1) == int(year)) or
+    (int(day1) == int(day) and int(month1) > int(month) and int(year1) > int(year)) or
+    (int(day1) > int(day) and int(month1) == int(month) and int(year1) > int(year)) or
+    (int(day1) > int(day) and int(month1) > int(month) and int(year1) > int(year))
+) :
+    print(f"Внимание! Дедлайн истёк {(int(day1) - int(day))+((int(month1) - int(month))*30)+((int(year1) - int(year))*365)} дня назад.")
 else:
-    print(f"До дедлайна осталось {create_date - issue_date} дня.")
+    print(f"До дедлайна осталось {(int(day) - int(day1))+((int(month) - int(month1))*30)+((int(year)-int(year1))*365)} дня.")
