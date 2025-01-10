69236b48559706982e2dc6ded7de5efeae76f88a
Andreipoh <pohlebaevandrei@yandex.ru>
2025-01-10 19:50:59 +0500

    Add files via upload

--- create_note_function.py
+++ create_note_function.py
@@ -1,11 +1,13 @@
+# Функция создания заметки
 from datetime import date
 def create_note():
     a = input('Введите имя пользователя: ')
     b = input('Введите заголовок заметки: ')
     c = input('Введите описание заметки: ')
     d = input('Введите статус заметки (новая, в процессе, выполнено): ')
-    e = date.today()
+    e = date.today() # Получаем автоматически текущую дату и помещаем её в словарь note как дату создания заметки
     f = input('Введите дедлайн (день-месяц-год): ')
-    print( { 'username': a, 'title': b, 'content': c, 'status': d, 'created_date ': f'{e.day}-{e.month}-{e.year}', 'issue_date': f })
+    note = { 'username': a, 'title': b, 'content': c, 'status': d, 'created_date ': f'{e.day}-{e.month}-{e.year}', 'issue_date': f } # Создаем словарь для заметки
+    print(note)
 create_note()
 print()
