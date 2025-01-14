584c7e03c7570b3bc1877a0791a470c3c14fac1a
Andreipoh <pohlebaevandrei@yandex.ru>
2025-01-14 22:04:44 +0500

    Add files via upload

--- add_titles_loop.py
+++ add_titles_loop.py
@@ -1,7 +1,11 @@
+titles = []
 while True:
     title = input('Введите заголовок (или оставьте пустым для завершения): ')
+    titles.append(title)
     if title == 'стоп':
         break
     if title == '':
         break
-    print(title)
+
+print(titles)
+
