da31deed0b16c46e9f2c33ab412368e2d1345c66
Andreipoh <pohlebaevandrei@yandex.ru>
2025-01-17 22:48:09 +0500

    Update check_deadline.py

--- check_deadline.py
+++ check_deadline.py
@@ -1,17 +1,26 @@
-from datetime import date
+from datetime import  datetime
 
-day,month,year = input('Введите дату дедлайна (в формате день-месяц-год "xx-xx-xxxx"): ').split('-')
-issue_date = (f'{day}-{month}-{year}')
-year1, month1, day1= str(date.today()).split('-')
-print(day1,month1,year1)
-if ((int(day1) > int(day) and int(month1) == int(month) and int(year1) == int(year)) or
-    (int(day1) == int(day) and int(month1) > int(month) and int(year1) == int(year)) or
-    (int(day1) == int(day) and int(month1) == int(month) and int(year1) > int(year)) or
-    (int(day1) > int(day) and int(month1) > int(month) and int(year1) == int(year)) or
-    (int(day1) == int(day) and int(month1) > int(month) and int(year1) > int(year)) or
-    (int(day1) > int(day) and int(month1) == int(month) and int(year1) > int(year)) or
-    (int(day1) > int(day) and int(month1) > int(month) and int(year1) > int(year))
-) :
-    print(f"Внимание! Дедлайн истёк {(int(day1) - int(day))+((int(month1) - int(month))*30)+((int(year1) - int(year))*365)} дня назад.")
-else:
-    print(f"До дедлайна осталось {(int(day) - int(day1))+((int(month) - int(month1))*30)+((int(year)-int(year1))*365)} дня.")
+current_date = datetime.today()
+print(f'Текущая дата: {current_date.strftime('%d-%m-%Y')}')
+
+while True:
+    issue_date = input('Введите дату дедлайна (в формате день-месяц-год "xx-xx-xxxx"): ')
+    try:
+        issue_date = datetime.strptime(issue_date,'%d-%m-%Y')
+        break
+    except ValueError:
+        print('Неверный формат даты')
+delta = issue_date - current_date
+days_difference = delta.days*1
+if delta == 1 :
+    print("Дедлайн уже завтра!")
+if 1 < days_difference < 5:
+    print(f"До дедлайна осталось {days_difference} дня.")
+if days_difference >= 5:
+    print(f"До дедлайна осталось {days_difference} дней.")
+if days_difference == -1:
+    print(f"Внимание! Дедлайн истёк {-(days_difference)} день назад!")
+if -1 > days_difference > -5:
+    print(f"Внимание! Дедлайн истёк {-(days_difference)} дня назад!")
+if days_difference <= -5:
+    print(f"Внимание! Дедлайн истёк {-(days_difference)} дней назад!")
